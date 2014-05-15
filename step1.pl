7b9160cf2cd2f975063c8b184af31de4ce0a83df
ianelletson <ianelletson@gmail.com>
2014-05-14 20:56:53 -0500

    GOT DAT STEP ONE SON

--- step1.pl
+++ step1.pl
@@ -13,6 +13,7 @@
 stmt([pass|More], More).
 stmt([declare,X|More], More) :- not(nonTerminal(X)).
 stmt([use,X|More], More) :-  not(nonTerminal(X)).
+stmt([begin|Block], More) :- blck([begin|Block], More).
 stmt(A) :- stmt(A, []).
 
 %% Statements
@@ -20,9 +21,18 @@ stmts([pass|More], More).
 stmts([Keyword, Var|More], More) :- stmt([Keyword,Var]).
 stmts([pass|More], Other) :- stmts(More,Other).
 stmts([Keyword, Var|More], Other) :- stmt([Keyword,Var]), stmts(More,Other).
+stmts([begin|Stmts], Tail) :- stmts(Stmts, [end|Tail]).
 stmts(A) :- stmts(A, []).
 
 %% Block
+%% blck([begin,declare,x,use,x,end,tail],[tail]).
+%% blck([begin,declare,x,begin,use,x,end,end,something,else],
+        %%[something,else]).
+%% blck([begin|Stmts]) :- stmts(Stmts, [end]). %% Works for no tail
+blck([begin,end|Tail], Tail).
+blck([begin|Stmts], Tail) :- stmts(Stmts, [end|Tail]).
+blck(Block) :- blck(Block, []).
+
 
 %% Facts
 nonTerminal(pass).
