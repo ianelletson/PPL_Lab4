fc6929d60bda4c8bde153ba2baed7e256d7c728d
ianelletson <ianelletson@gmail.com>
2014-05-14 21:04:01 -0500

    Now ACTUALLY done with stage 1 son!

--- step1.pl
+++ step1.pl
@@ -13,7 +13,7 @@
 stmt([pass|More], More).
 stmt([declare,X|More], More) :- not(nonTerminal(X)).
 stmt([use,X|More], More) :-  not(nonTerminal(X)).
-stmt([begin|Block], More) :- blck([begin|Block], More).
+stmt(Block, More) :- blck(Block, More).
 stmt(A) :- stmt(A, []).
 
 %% Statements
@@ -21,7 +21,7 @@ stmts([pass|More], More).
 stmts([Keyword, Var|More], More) :- stmt([Keyword,Var]).
 stmts([pass|More], Other) :- stmts(More,Other).
 stmts([Keyword, Var|More], Other) :- stmt([Keyword,Var]), stmts(More,Other).
-stmts([begin|Stmts], Tail) :- stmts(Stmts, [end|Tail]).
+stmts([begin|Stmts], More) :- stmts(Stmts, [end|More]).
 stmts(A) :- stmts(A, []).
 
 %% Block
@@ -29,6 +29,8 @@ blck([begin,end|Tail], Tail).
 blck([begin|Stmts], Tail) :- stmts(Stmts, [end|Tail]).
 blck(Block) :- blck(Block, []).
 
+%% Legal
+legal(L) :- blck(L,[]).
 
 %% Facts
 nonTerminal(pass).
