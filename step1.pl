0fd1439a4d031fc2d03f44dba1e0d07f968b8602
ianelletson <ianelletson@gmail.com>
2014-05-17 22:48:46 -0500

    Working on stage 1 bug.

--- step1.pl
+++ step1.pl
@@ -2,7 +2,7 @@
 %% <blck> ::= begin <stmts> end
 %% 
 %% <stmts> ::= <empty>
-          %% | <stmt> <stmts>
+%%          | <stmt> <stmts>
 %% 
 %% <stmt> ::= pass
 %%          | declare <name>
@@ -18,12 +18,13 @@ stmt(A) :- stmt(A, []).
 
 %% Statements
 stmts([pass|More], More).
-stmts([Keyword, Var|More], More) :- stmt([Keyword,Var]).
 stmts([pass|More], Other) :- stmts(More,Other).
+stmts([Keyword, Var|More], More) :- stmt([Keyword,Var]).
 stmts([Keyword, Var|More], Other) :- stmt([Keyword,Var]), stmts(More,Other).
 stmts([begin|Stmts], More) :- stmts(Stmts, [end|More]).
 stmts(A) :- stmts(A, []).
 
+%% blck([begin,pass,begin,pass,end,pass,end]).
 %% Block
 blck([begin,end|Tail], Tail).
 blck([begin|Stmts], Tail) :- stmts(Stmts, [end|Tail]).
